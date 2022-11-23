09c6e6bf1b9810f67c38cc915c9173d3a3fbb415
Harry Lumb <harrylumbb@gmail.com>
2022-11-23 12:02:24 +0000

    Update Books.jsx

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,23 +1,30 @@
+import { Component } from 'react'
 import { Card, Container, Row, Col, Button } from 'react-bootstrap'
-import HistoryBooks from '../data/history.json'
+import historyBooks from '../data/history.json'
 
-const Books = () => (
-  <Container>
-    <Row>
-      {HistoryBooks.map((book) => (
-        <Col className="mb-2" lg={3}>
-          <Card style={{ width: '250px', height:"600px" }}>
-            <Card.Img variant="top" src={book.img} />
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-              <Card.Text>
-                <Button>Buy: ${book.price}</Button>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  </Container>
-)
+class Books extends Component{
+    render() {
+        return (
+            <Container>
+            <Row>
+              {historyBooks.map(book => (
+                <Col key={book.asin} className="mb-2" lg={3} md={4} sm={6} xs={12}>
+                  <Card style={{ width: '250px', height:"600px" }}>
+                    <Card.Img variant="top" src={book.img} />
+                    <Card.Body>
+                      <Card.Title>{book.title}</Card.Title>
+                      <Card.Text>
+                        <Button>Buy: ${book.price}</Button>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+       ) }
+
+        
+    }
+ 
 export default Books
